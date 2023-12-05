c5f1981804c777c57ceb599fa39fed84784dea70
Hamza <bakkar.hamza92@gmail.com>
2023-12-05 19:26:01 +0100

    Reviews App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,9 @@ import NewPassword from './pages/Auth/NewPassword';
 import AuthSuccess from './pages/Auth/AuthSuccess';
 import LogoutSuccess from './pages/Auth/LogoutSuccess';
 import Home from './pages/Home';
-import Portfolio from './pages/portfolio';
 import Boutiques from "./pages/Boutiques/index";
 import ShowBoutique from './components/Boutique/show';
+import Portfolio from './pages/Portfolio';
 import Carts from './components/Cart/index';
 import LegalSection from './components/Footer/LegalSection';
 import PrivacyPolicySection from './components/Footer/PrivacyPolicySection';
@@ -31,10 +31,10 @@ import Footer from './components/Footer';
 
 function App() {
   const [, setUser] = useAtom(userAtom);
-
+  
   useEffect(() => {
     const token = Cookies.get('token');
-
+    
     if (token) {
       setUser({
         isLoggedIn: true,
@@ -42,36 +42,36 @@ function App() {
       });
     }
   }, []);
-
+  
   return (
     <>
-      <div className="flex flex-col min-h-screen">
-        <Router>
-          <NavBar />
-          <ToastContainer />
-          <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/property/:id" element={<ShowProperty />} />
-              <Route path="/boutique" element={<Boutiques/>}/>
-              <Route path="/myproperties/:id" element={<MyProperties />} />
-              <Route path="/createproperty" element={<CreateProperty />} />
-              <Route path="updateproperty/:id" element={<UpdateProperty />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/newpassword" element={<NewPassword />} />
-              <Route path="/authsuccess" element={<AuthSuccess />} />
-              <Route path="/logoutsuccess" element={<LogoutSuccess />} />
-              <Route path="/mentions-legales" element={<LegalSection />} />
-              <Route path="/politique-confidentialite" element={<PrivacyPolicySection />} />
-              <Route path="/politique-remboursement" element={<RefundPolicySection />} />
-            </Routes>
-          </main>
-          <Footer />
-        </Router>
-      </div>
-    </>
-  );
+    <div className="flex flex-col min-h-screen">
+      <Router>
+        <NavBar />
+        <ToastContainer />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/boutique" element={<Boutiques/>}/>
+            <Route path="/cartitems" element={<Carts/>}/>
+            <Route path="/item/:itemId" element={<ShowBoutique />} />
+            <Route path="/register" element={<Register/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/newpassword" element={<NewPassword/>} />
+            <Route path="/authsuccess" element={<AuthSuccess/>} />
+            <Route path="/logoutsuccess" element={<LogoutSuccess/>} />
+            <Route path="/mentions-legales" element={<LegalSection />} />
+            <Route path="/politique-confidentialite" element={<PrivacyPolicySection />} />
+            <Route path="/politique-remboursement" element={<RefundPolicySection />} />
+            <Route path="/faq" element={<Faq />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </div>
+  </>
+);
 }
-
-export default App;
+  
+  export default App;

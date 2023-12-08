88b9ec5313ad1db2e2ec0175873e353a4bbdf3a7
Hamza <bakkar.hamza92@gmail.com>
2023-12-08 10:41:06 +0100

    API URL PRODUCTION AND LOCAL

--- src/stores/apiUrl.js
+++ src/stores/apiUrl.js
@@ -1,2 +1,11 @@
+/* eslint-disable no-undef */
 // export const API_URL = "http://127.0.0.1:3000";
-export const API_URL = "http://127.0.0.1:3000"
+import urljoin from 'url-join';
+
+const API_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3001/'
+    : 'https://rustoffback-d3677a58098b.herokuapp.com/';
+
+const finalUrl = urljoin(API_URL, '/');
+export default finalUrl;

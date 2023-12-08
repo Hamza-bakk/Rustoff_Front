fae1a182c064e26e33c412a81905a7cc17c1f1e8
Hamza <bakkar.hamza92@gmail.com>
2023-12-08 10:45:44 +0100

    api url review

--- src/stores/apiUrl.js
+++ src/stores/apiUrl.js
@@ -1,11 +1,2 @@
-/* eslint-disable no-undef */
 // export const API_URL = "http://127.0.0.1:3000";
-import urljoin from 'url-join';
-
-const API_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3001/'
-    : 'https://rustoffback-d3677a58098b.herokuapp.com/';
-
-const finalUrl = urljoin(API_URL, '/');
-export default finalUrl;
+export const API_URL = "http://127.0.0.1:3000"
